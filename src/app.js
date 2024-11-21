27acb041bb488a50fe6fcebf41103c6f957bb8e7
RdeRausseo <amrausseo20@gmail.com>
2024-11-21 21:53:12 +0000

    trabajando events form validation

--- src/app.js
+++ src/app.js
@@ -7,5 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const form = document.getElementById("formExample");
+  let flag = true;
+
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    let card = document.getElementById("inputCard");
+    if (card.value.length < 16 || card.value.length > 20) {
+      card.classList.add("bg-danger-subtle");
+      flag = false;
+    }
+    let cvc = document.getElementById("inputCvc");
+    if (cvc.value.length < 3 || cvc.value.length > 4) {
+      cvc.classList.add("bg-danger-subtle");
+      flag = false;
+    }
+    let amount = document.getElementById("inputAmount");
+    if (amount.value.length == "") {
+      amount.classList.add("bg-danger-subtle");
+    }
+    let name = document.getElementById("inputName");
+    let testname = new RegExp("");
+    if (name.value )
+    let lastName = document.getElementById("inputLastName");
+    if (flag) e.target.submit();
+  });
 };
+
+/* if (e.cancelable) {
+    if (document.getElementById("inputCard").value.length < 10) {
+    inputmoment = document
+      .getElementById("inputCard")
+      .classList.add("bg-danger-subtle");
+  }
+} */
